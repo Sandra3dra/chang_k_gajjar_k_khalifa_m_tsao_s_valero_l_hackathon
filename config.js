7bff5906104c575378a34d2be89294a28111490e
SandraTsao <43250383+SandraTsao@users.noreply.github.com>
2019-12-05 00:45:02 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     uname: "b71f531e61ab76",
     upass: "5778d0e6", // windows users leave this blank
     port: 8889, // 3306 for windows or linux
-    host: "cleardb.net",
+    host: "us-cdbr-iron-east-05.cleardb.net",
     db: "heroku_16cad15fc6fd2d3"
 }
 
