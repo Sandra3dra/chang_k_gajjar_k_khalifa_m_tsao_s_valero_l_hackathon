aab750fb1fe9b8d3179599fa6ac4b25200e353d3
SandraTsao <43250383+SandraTsao@users.noreply.github.com>
2019-12-05 01:21:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    uname: "b71f531e61ab76",
-    upass: "5778d0e6", // windows users leave this blank
+    uname: "b716c7b0614e5a",
+    upass: "6ab4501c", // windows users leave this blank
     port: 8889, // 3306 for windows or linux
     host: "us-cdbr-iron-east-05.cleardb.net",
-    db: "heroku_16cad15fc6fd2d3"
+    db: "heroku_0f75674ec1cd0d9"
 }
 
 module.exports = config;
